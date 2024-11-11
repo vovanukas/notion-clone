2d655387d784bc2bbbc26c37051ff5ace126feff
Vladimiras Malyskinas <malvladimiras@gmail.com>
2024-11-11 08:05:27 +0100

    Added archiving function for pages

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,51 @@
+"use client";
+
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+import { useUser } from "@clerk/clerk-react";
+import { PlusCircleIcon } from "lucide-react";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
+
 const DocumentsPage = () => {
+    const { user } = useUser();
+    const create = useMutation(api.documents.create);
+
+    const onCreate = () => {
+        const promise = create({ title: "Untitled" });
+
+        toast.promise(promise, {
+            loading: "Creating a new note...",
+            success: "New note created!",
+            fail: "Failed to create a new note.",
+        })
+    }
+
     return ( 
-        <div>
-            This is a protected page.
+        <div className="h-full flex flex-col items-center justify-center space-y-4">
+            <Image
+                src="/empty.png"
+                height="300"
+                width="300"
+                alt="Empty"
+                className="dark:hidden"
+            />
+            <Image
+                src="/empty-dark.png"
+                height="300"
+                width="300"
+                alt="Empty"
+                className="hidden dark:block"
+            />
+            <h2 className="text-lg font-medium">
+                Welcome to { user?.firstName } &apos;s Jotion!
+            </h2>
+            <Button onClick={onCreate}>
+                <PlusCircleIcon className="h-4 w-4 mr-2" />
+                Create a note
+            </Button>
         </div>
      );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ConvexClientProvider } from "@/components/providers/convex-provider";
+import { Toaster } from "sonner";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -52,6 +53,7 @@ export default function RootLayout({
               disableTransitionOnChange
               storageKey="jotion-theme"
             >
+              <Toaster position="bottom-center" />
               {children}
             </ThemeProvider>
           </ConvexClientProvider>
